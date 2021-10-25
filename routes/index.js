b10ed6f16d244d2675c53f937b246791d7a35b7c
Spookeletons <84mo@fresnou.org>
2021-10-25 08:59:46 -0700

    my initial commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
+router.post('/results', function(req, res) {
+  console.log(req.body.description);
+  console.log(req.body.agree);
+  console.log(req.body.iceCream);
+  console.log(req.body.iceCreamOther);
+  res.render('index');
+});
 module.exports = router;
+router.post('/yourTurn', function(req,res) {
+  drink: req.body.
+})
