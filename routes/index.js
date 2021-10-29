38d235fa363db76912c5ff9e77cc1ea80d250014
Spookeletons <84mo@fresnou.org>
2021-10-29 16:14:26 -0700

    added all but return values

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,45 @@ router.post('/results', function(req, res) {
   res.render('index');
 });
 module.exports = router;
-router.post('/yourTurn', function(req,res) {
-  drink: req.body.
+router.post('/order', function(req,res) {
+  // console.log(req.body.order);
+  // console.log(req.body.drink);
+  // console.log(req.body.pizza);
+  // console.log(req.body.free1);
+  // console.log(req.body.free1);
+  // console.log(req.body.free2);
+  // console.log(req.body.free3);
+  // console.log(req.body.free4);
+  // console.log(req.body.free5);
+  // console.log(req.body.topping1);
+  // console.log(req.body.topping2);
+  // console.log(req.body.toppings);
+  let body = req.body;
+  let order = getOrder(body);
+  res.render('order', {
+    order: order
+  });
 })
+
+function getOrder(formData){
+  var choice;
+  if (formData.order === "one"){
+    return giveOrder1(formData);
+  } else if (formData.order === "two"){
+    return giveOrder2(formData);
+  } else if (formData.order === "king"){
+    return giveOrder3(formData);
+  } else {
+    message = "try again";
+    res.render('index');
+  }
+}
+function giveOrder1(formData){
+  return ``;
+}
+function giveOrder2(formData){
+  return `One ${formData.drink}`;
+}
+function giveOrder3(formData){
+  return '';
+}
